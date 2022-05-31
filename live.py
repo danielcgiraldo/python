83556f32172a90ac098b7511bccdb3f099c48798
Daniel Castillo Giraldo <dc.giraldocg@hotmail.com>
2022-05-30 20:59:32 -0500

    Strings and Files

--- live.py
+++ live.py
@@ -1,20 +1,3 @@
-def generar_julianachi(max):
-    julianachi = [1, 2]
-    num = 2
-    while num<max:
-        divisores = 2
-        for i in range(2, julianachi[-1] // 2 + 1):
-            if(julianachi[-1] % i == 0):
-                divisores += 1
-        julianachi.append(julianachi[-1] + divisores)
-        num = julianachi[-1]
-    return julianachi
-
-while 6>5:
-    x = int(input())
-    if(x == 0):
-        break
-    if x in generar_julianachi(x):
-        print('Pertenece a la serie de Julianachi')
-    else:
-        print("No pertenece a la serie de Julianachi")
+a = open('./resources/haiku.txt', 'a')
+print('\nHola\nMundo', end='', file=a)
+a.close()
